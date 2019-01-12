56af8f0b1ef8377671f008bceade610df21dce28
AMHR <amhr_1997@hotmail.com>
2019-01-12 00:21:51 -0600

    actualizacion cinco heroku

--- POne/server.js
+++ POne/server.js
@@ -7,10 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/POne'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/POne/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/POne/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, (err) => {
+    if (err) throw err;
+    console.log('Server Ok.');
+});
